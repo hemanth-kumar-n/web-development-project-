6ce10b86d38981246743f045e81298621f37dd08
hemanth-kumar-n <133811060+hemanth-kumar-n@users.noreply.github.com>
2023-06-10 11:30:32 +0530

    Add files via upload

---
+++ moviesweb/script.js
@@ -0,0 +1,55 @@
+
+const API_KEY = 'api_key=9b88799409a0185188babde9a6c00792';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+
+
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
+
+
+
+getMovies(API_URL);
+
+function getMovies(url) {
+  
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+            showMovies(data.results);
+       
+    })
+
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${IMG_URL+poster_path }" alt="${title}">
+             <div class="movie-info">
+          <h3>${title}</h3>
+        </div>
+            `
+        main.appendChild(movieEl);
+        })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+
+})
+
